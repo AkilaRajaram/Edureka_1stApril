ed928f8bb6323be3e6f340630729381cecf8ed2f
AkilaRajaram <49613829+AkilaRajaram@users.noreply.github.com>
2019-04-14 12:54:25 -0400

    Name Change removed

--- src/module1/FirstTestClass.java
+++ src/module1/FirstTestClass.java
@@ -4,7 +4,7 @@ public class FirstTestClass {
 
 	public static void main(String[] args) {
 		
-      System.out.println("Welcome to Edureka's instructor led training by Abhresh Trainer");	
+      System.out.println("Welcome to Edureka's instructor led training by Abhresh");	
       
       //System.out.println("Welcome to Edureka's instructor led training by Abhresh");	
       
